01a9b5631d207ef8cefd0b84f2af994e369e2c17
yasuda <samonkntd@gmail.com>
2025-09-30 23:25:47 +0900

    feat: components recipient 登録編集フォーム

--- components/protected/recipients/RecipientEditForm.tsx
+++ components/protected/recipients/RecipientEditForm.tsx
@@ -198,35 +198,57 @@ export default function RecipientEditForm({ recipientId, initialData, onCancel }
         },
         contactAddress: {
           address: initialData.detail?.address || '',
-          formOfResidence: (initialData.detail as any)?.formOfResidence || initialData.detail?.form_of_residence || '',
-          formOfResidenceOtherText: (initialData.detail as any)?.formOfResidenceOtherText || initialData.detail?.form_of_residence_other_text || '',
-          meansOfTransportation: (initialData.detail as any)?.meansOfTransportation || initialData.detail?.means_of_transportation || '',
-          meansOfTransportationOtherText: (initialData.detail as any)?.meansOfTransportationOtherText || initialData.detail?.means_of_transportation_other_text || '',
+          formOfResidence: initialData.detail?.formOfResidence || initialData.detail?.form_of_residence || '',
+          formOfResidenceOtherText: initialData.detail?.formOfResidenceOtherText || initialData.detail?.form_of_residence_other_text || '',
+          meansOfTransportation: initialData.detail?.meansOfTransportation || initialData.detail?.means_of_transportation || '',
+          meansOfTransportationOtherText: initialData.detail?.meansOfTransportationOtherText || initialData.detail?.means_of_transportation_other_text || '',
           tel: initialData.detail?.tel || '',
         },
-        emergencyContacts: (initialData.detail?.emergency_contacts?.length ? initialData.detail.emergency_contacts : [{}]).map((contact: any) => ({
-          firstName: contact.firstName || contact.first_name || '',
-          lastName: contact.lastName || contact.last_name || '',
-          firstNameFurigana: contact.firstNameFurigana || contact.first_name_furigana || '',
-          lastNameFurigana: contact.lastNameFurigana || contact.last_name_furigana || '',
-          relationship: contact.relationship || '',
-          tel: contact.tel || '',
-          address: contact.address || '',
-          notes: contact.notes || '',
-          priority: contact.priority || 1,
-        })),
+        emergencyContacts: (initialData.detail?.emergency_contacts?.length
+          ? initialData.detail.emergency_contacts.map(contact => ({
+              firstName: contact.firstName || contact.first_name || '',
+              lastName: contact.lastName || contact.last_name || '',
+              firstNameFurigana: contact.firstNameFurigana || contact.first_name_furigana || '',
+              lastNameFurigana: contact.lastNameFurigana || contact.last_name_furigana || '',
+              relationship: contact.relationship || '',
+              tel: contact.tel || '',
+              address: contact.address || '',
+              notes: contact.notes || '',
+              priority: contact.priority || 1,
+            }))
+          : [{
+              firstName: '',
+              lastName: '',
+              firstNameFurigana: '',
+              lastNameFurigana: '',
+              relationship: '',
+              tel: '',
+              address: '',
+              notes: '',
+              priority: 1,
+            }]
+        ),
         disabilityInfo: {
-          disabilityOrDiseaseName: (initialData.disability_status as any)?.disabilityOrDiseaseName || initialData.disability_status?.disability_or_disease_name || '',
-          livelihoodProtection: (initialData.disability_status as any)?.livelihoodProtection || initialData.disability_status?.livelihood_protection || '',
-          specialRemarks: (initialData.disability_status as any)?.specialRemarks || initialData.disability_status?.special_remarks || '',
+          disabilityOrDiseaseName: initialData.disability_status?.disabilityOrDiseaseName || initialData.disability_status?.disability_or_disease_name || '',
+          livelihoodProtection: initialData.disability_status?.livelihoodProtection || initialData.disability_status?.livelihood_protection || '',
+          specialRemarks: initialData.disability_status?.specialRemarks || initialData.disability_status?.special_remarks || '',
         },
-        disabilityDetails: (initialData.disability_status?.details?.length ? initialData.disability_status.details : [{}]).map((detail: any) => ({
-          category: detail.category || '',
-          gradeOrLevel: detail.gradeOrLevel || detail.grade_or_level || '',
-          physicalDisabilityType: detail.physicalDisabilityType || detail.physical_disability_type || '',
-          physicalDisabilityTypeOtherText: detail.physicalDisabilityTypeOtherText || detail.physical_disability_type_other_text || '',
-          applicationStatus: detail.applicationStatus || detail.application_status || '',
-        })),
+        disabilityDetails: (initialData.disability_status?.details?.length
+          ? initialData.disability_status.details.map(detail => ({
+              category: detail.category || '',
+              gradeOrLevel: detail.gradeOrLevel || detail.grade_or_level || '',
+              physicalDisabilityType: detail.physicalDisabilityType || detail.physical_disability_type || '',
+              physicalDisabilityTypeOtherText: detail.physicalDisabilityTypeOtherText || detail.physical_disability_type_other_text || '',
+              applicationStatus: detail.applicationStatus || detail.application_status || '',
+            }))
+          : [{
+              category: '',
+              gradeOrLevel: '',
+              physicalDisabilityType: '',
+              physicalDisabilityTypeOtherText: '',
+              applicationStatus: '',
+            }]
+        ),
       };
 
 

b0dc733dbe202cf79ba56e26defd48934d421e7e
yasuda <samonkntd@gmail.com>
2025-11-11 14:48:49 +0900

    feat: notice 通知ページ

---
+++ app/(protected)/notice/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import NotificationsTab from '@/components/notice/NotificationsTab';
+
+export default function NoticePage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <NotificationsTab />
+    </div>
+  );
+}
